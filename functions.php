d6a861126956f82ad6a79d53cabd53cdfb02d683
Geoff Shapiro <gshapiro49@gmail.com>
2014-03-22 02:13:06 -0400

    Updated the XML location
    Now it’s back on PAs servers so they can update it and we don’t have to
    keep a local copy

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?PHP
 
 $START_DAY = "2014-04-11";
-$XML_LOCATION = "GuidebookSchedule.xml";
+$XML_LOCATION = "http://hw1.pa-cdn.com/pax/resources/GuidebookSchedule.xml";
 /**
  * makeICal creates the ical for the given events
  * @param events Events to turn into ical
